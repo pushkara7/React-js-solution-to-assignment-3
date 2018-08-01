3bf6134b88fef9560f7e55bb68faef59a6d66336
pushkara7 <pushkaragona@gmail.com>
2018-07-31 23:41:20 -0700

    initial commit

---
+++ src/containers/Users/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
+
+class Users extends Component {
+    render () {
+        return (
+            <div>
+                <h1>The Users Page</h1>               
+            </div>
+
+        );
+    }
+}
+
+export default Users;
